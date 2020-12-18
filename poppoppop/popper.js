c5cd06a13b456bbb7e705ce159acb799ed039c81
akabomb-prog <69766525+akabomb-prog@users.noreply.github.com>
2020-12-18 21:09:19 +0200

    Update Pop pop pop
    Prevent zooming on mobile
    Move the script to a separate file

---
+++ poppoppop/popper.js
@@ -0,0 +1,54 @@
+
+/* Initial setup */
+
+console.log("What's poppin'?");
+
+var pop = document.createElement("audio");
+	
+pop.src = "./o0.wav";
+pop.setAttribute("preload", "auto");
+pop.setAttribute("controls", "none");
+pop.style.display = "none";
+document.body.appendChild(pop);
+
+var cat = document.createElement("img");
+cat.align = "middle";
+cat.src = "./pCp.jpg";
+
+cat.width = 512;
+cat.height = 512;
+
+/* Page setup */
+
+function play()
+{
+	pop.play()
+		.catch(e =>
+		{
+			console.log("Can't play sound.", e);
+		});
+}
+
+cat.oncontextmenu = function ()
+{
+	return false;
+};
+cat.onpointerdown = function ()
+{
+	pop.pause();
+	cat.src = "./pOp.jpg";
+	pop.src = "./o" + Math.floor(Math.random() * 11) + ".wav";
+	play();
+};
+cat.onpointerup = function ()
+{
+	if (cat.src.endsWith("pCp.jpg")) return;
+	
+	pop.pause();
+	cat.src = "./pCp.jpg";
+	pop.src = "./c" + Math.floor(Math.random() * 12) + ".wav";
+	play();
+};
+cat.onpointerleave = cat.onpointerup;
+
+document.body.insertBefore(cat, document.body.childNodes[0]);
