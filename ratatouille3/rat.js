ce66448cf64cc4bc1f5c68322e285383f82e645c
akabomb-prog <69766525+akabomb-prog@users.noreply.github.com>
2021-02-23 23:40:50 +0200

    Update The Rat
    Create folders for files
    Add dragging rat to other spots

--- ratatouille3/rat.js
+++ ratatouille3/rat.js
@@ -2,13 +2,13 @@
 // This code could be improved, but this is just for a one-off joke so who cares
 
 const cursorInfo = {
-	empty: "hanada.png",
-	rat: "ratata.png"
+	empty: "cursor/hanada.png",
+	rat: "cursor/ratata.png"
 };
 
 const soundInfo = {
-	grab: "rote_away.wav",
-	back: "putback.wav"
+	grab: "sound/rote_away.wav",
+	back: "sound/putback.wav"
 };
 
 var rat;
@@ -16,6 +16,30 @@ var ratinplace = true;
 
 var statusText;
 var statusTimeout = 0;
+
+function handleRat(event)
+{
+	if (ratinplace) return;
+	
+	ratinplace = false;
+	
+	var x = event.clientX, y = event.clientY;
+	var ratwidth = rat.style.width.substring(0, rat.style.width.length - 2);
+	setRatVisible(true);
+	
+	console.log(ratwidth);
+	
+	rat.style.top = y + 'px';
+	rat.style.left = (x - ratwidth) + 'px';
+	
+	playonce(soundInfo.back);
+	setRatVisible(true);
+	setBodyCursor(cursorInfo.empty);
+	youHave("put the rat back");
+	
+	console.log("rat placed on " + x + " " + y);
+	event.preventDefault();
+}
 
 function youHave(havewhat)
 {
@@ -28,10 +52,12 @@ function youHave(havewhat)
 	}, 4000);
 }
 
-function getRatAndStatus()
+function bodyload()
 {
 	rat = document.getElementById("rat");
+	rat.style.width = 200;
 	statusText = document.getElementById("status");
+	document.body.onpointerup = handleRat;
 }
 
 function setRatCursor(cur)
@@ -39,11 +65,21 @@ function setRatCursor(cur)
 	rat.style.cursor = `url(${cur}), auto`;
 }
 
-function toggleRat()
+function setRatVisible(vis)
 {
-	ratinplace = !ratinplace;
-	playonce(ratinplace ? soundInfo.back : soundInfo.grab);
-	rat.style.opacity = ratinplace ? 100 : 0;
-	setRatCursor(ratinplace ? cursorInfo.empty : cursorInfo.rat);
-	youHave(ratinplace ? "put the rat back" : "taken the rat");
+	rat.style.visibility = vis ? "visible" : "hidden";
+}
+
+function setBodyCursor(cur)
+{
+	document.body.style.cursor = `url(${cur}), auto`;
+}
+
+function takeRat()
+{
+	ratinplace = false;
+	playonce(soundInfo.grab);
+	setRatVisible(false);
+	setBodyCursor(cursorInfo.rat);
+	youHave("taken the rat");
 }
