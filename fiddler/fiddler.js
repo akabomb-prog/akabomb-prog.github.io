6c4abb0d81b446ac0b9de0d76de83d2ec6cf4507
akabomb-prog <69766525+akabomb-prog@users.noreply.github.com>
2021-04-25 14:25:24 +0300

    Add Infinite Fiddler Crab

---
+++ fiddler/fiddler.js
@@ -0,0 +1,17 @@
+function populate()
+{
+	// Safeguard: appending image doesn't seem to update the page height, even when assigning populate to the created image
+	for (var i = 0; i < 5; i++)
+	{
+		var pageBottom = document.documentElement.getBoundingClientRect().bottom;
+		console.log(pageBottom);
+		if (pageBottom > 738 * 8) return;
+		
+		var img = document.createElement('img');
+		img.src = "fiddler_loop.png";
+		
+		document.body.appendChild(img);
+	}
+}
+
+window.addEventListener("scroll", populate);
