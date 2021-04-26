6d1f182b7ab3c828040014c69a1f9ba9f5fb1174
akabomb-prog <69766525+akabomb-prog@users.noreply.github.com>
2021-04-26 10:42:41 +0300

    Hopefully fix Fiddler for mobile

--- fiddler/fiddler.js
+++ fiddler/fiddler.js
@@ -1,7 +1,7 @@
 function populate()
 {
 	// Safeguard: appending image doesn't seem to update the page height, even when assigning populate to the created image
-	for (var i = 0; i < 5; i++)
+	for (var i = 0; i < 16; i++)
 	{
 		var pageBottom = document.documentElement.getBoundingClientRect().bottom;
 		console.log(pageBottom);
